b788becf5617b2befdfb8d64b99dadb7e6c383f4
KwonHyukIl <gurdlf320@naver.com>
2025-07-21 18:17:10 +0900

    10809 complete

--- baeckjoon/2025_07/10809.py
+++ baeckjoon/2025_07/10809.py
@@ -1,17 +1,10 @@
-# word = list(map(str, input()))
+word = list(map(str, input()))
 
-# alphabet = [chr(i) for i in range(97,123)]
-# # a = 1 z = 공백
+alphabet = [chr(i) for i in range(97,123)]
+# a = 1 z = 공백
 
-# print(word)
-# print(alphabet)
-
-
-# for i in range(len(word)):
-#   print(i, end=" ")
-#   for j in range(len(alphabet)):
-#     if (word[i] == alphabet[j]):
-#       print("해당")
-#       alphabet[j] = j
-
-# print(alphabet)
+for i in alphabet:
+  if i in word:
+    print(word.index(i), end=' ')
+  else:
+    print(-1, end=' ')
