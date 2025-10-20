ddc2d5ec268cc0229d8c2feb925db364e3e969a3
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-10-20 21:53:01 +0900

    2차원 배열 덧셈 2

--- baeckjoon/2025_10/2738.py
+++ baeckjoon/2025_10/2738.py
@@ -1,39 +1,40 @@
-# N, M = list(map(int, input("N, M 입력: ").split()))
-
-# List_A = []
-
-# for A in range(N):
-#   row_A = list(map(int, input().split()))
-#   List_A.append(row_A)
-
-# result = []
-
-# for n in range(N):
-#   row_B = list(map(int, input().split()))
-  
-#   for m in range(M):
-#     add = List_A[n][m] + row_B[m]
-#     result.append(add)
-
-# for i in range(len(result)):
-#   print(result[i], end=' ')
-#   if (i + 1) % M == 0:
-#     print()
-
 N, M = list(map(int, input().split()))
 
 List_A = []
 
-for n in range(N):
+for A in range(N):
   row_A = list(map(int, input().split()))
   List_A.append(row_A)
-  
-for n in range(N):
+
+List_B = []
+
+for B in range(N):
   row_B = list(map(int, input().split()))
-  
+  List_B.append(row_B)
+
+for n in range(N):
   result = []
   for m in range(M):
-    add = List_A[n][m] + row_B[m]
+    add = List_A[n][m] + List_B[n][m]
     result.append(add)
-    
+  
   print(*result)
+
+
+# N, M = list(map(int, input().split()))
+
+# List_A = []
+
+# for n in range(N):
+#   row_A = list(map(int, input().split()))
+#   List_A.append(row_A)
+  
+# for n in range(N):
+#   row_B = list(map(int, input().split()))
+  
+#   result = []
+#   for m in range(M):
+#     add = List_A[n][m] + row_B[m]
+#     result.append(add)
+    
+#   print(*result)
